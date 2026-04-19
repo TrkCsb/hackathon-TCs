e593f53e4a572b1bc6b41055a7b0be1ef8dc3383
Török Csaba <diak_124.torok.csaba.gergo@10.c.2025.com>
2026-04-19 15:24:17 +0200

    "Magic numbers" task

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 
+def nextMagicNum(n: int) -> int:
+    beginning = str(n + 1)
+    length, half = len(beginning), (len(beginning) + 1) // 2
+
+    def pal(p, n):
+        return int(p + p[::-1]) if n % 2 == 0 else int(p + p[-2::-1])
+
+    if (c := pal(beginning[:half], length)) >= n + 1:
+        return c
+
+    p = str(int(beginning[:half]) + 1)
+    if len(p) > half:
+        h2 = (length + 2) // 2
+        return pal(str(10 ** (h2 - 1)), length + 1)
+    return pal(p, length)
+
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
-
+    for line in Path("input.txt").read_text(encoding="utf-8").splitlines():
+        if start := line.strip():
+            b, e = start.split("^") if "^" in start else (start, None)
+            print(nextMagicNum(int(b) ** int(e) if e else int(b)))
 
 if __name__ == "__main__":
     main()
